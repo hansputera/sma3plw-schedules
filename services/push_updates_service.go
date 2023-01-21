da2be0f04a2b597a39057a8af698f6df390a36eb
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2023-01-21 14:28:59 +0000

    feat(auto): push updates

--- services/push_updates_service.go
+++ services/push_updates_service.go
@@ -4,16 +4,38 @@ import (
 	"errors"
 	"os"
 	"path"
+	"time"
 
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/object"
 	"github.com/go-git/go-git/v5/plumbing/transport/ssh"
 )
 
 func PushUpdatesGit() error {
-	repo, err := git.PlainOpen(".git")
+	repo, err := git.PlainOpen(".")
 	if err != nil {
 		return err
 	}
+
+	worktree, err := repo.Worktree()
+	if err != nil {
+		return err
+	}
+
+	worktree.AddGlob(".")
+
+	c, err := worktree.Commit("feat(auto): push updates", &git.CommitOptions{
+		Author: &object.Signature{
+			Name:  "Hanif Dwy Putra S",
+			Email: "hanifdwyputrasembiring@gmail.com",
+			When:  time.Now(),
+		},
+	})
+	if err != nil {
+		return err
+	}
+
+	repo.CommitObject(c)
 
 	homedir, err := os.UserHomeDir()
 	if err != nil {
